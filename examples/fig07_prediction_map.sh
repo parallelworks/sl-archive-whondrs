686b53a7f5e78c3f7b6155b534f51cfb3bdeea06
StefanGary <sfgary@parallelworks.com>
2024-03-12 17:14:48 +0000

    Updated all figs to include S19S-SSS-log10-extrap-r03
    Error in S19S-SSS-log10-extrap-r02 required rerunning
    a new ML model ensemble. That was done on S19S-SSS-log10-extrap-r03
    and the downstream figures and intermediate files were updated.
    
    Also adjusted Fig. 7 to remove the bottom panel that does not
    provide much additional information.

--- examples/fig07_prediction_map.sh
+++ examples/fig07_prediction_map.sh
@@ -45,23 +45,29 @@ echo Plot predictions at GLORICH/WHONDRS sites...
 # Background is topography
 #================================================
 
+# Lines commented out with #-# are removing the
+# stream order (bottom) panel but for keeping
+# the predictions-at-river-network panel (top).
+# You should be able to recover the first panel
+# by uncommenting the #-# lines. 
+
 # Background
-gmt psbasemap -JM5i -R${ropt} -Ba2/a2WeSn -P -K -Y2i > $outps
+gmt psbasemap -JM5i -R${ropt} -Ba1/a1WeSn -P -K -Y2i > $outps
 gmt grdimage /work/ETOPO1_Ice_g_gmt4.grd -J -R -B -P -O -K -Ctopo.cpt.tmp >> $outps
 
 # River network (manually cloned https://github.com/parallelworks/global-river-databases here)
 tile_files1=/work/global-river-databases/RiverAtlas/tiles_compressed/RiverATLAS_v10_na.xyz.7411
 tile_files2=/work/global-river-databases/RiverAtlas/tiles_compressed/RiverATLAS_v10_na.xyz.7412
-gunzip -c ${tile_files1}.gz > $tile_files1
-gunzip -c ${tile_files2}.gz > $tile_files2
-gmt psxy $tile_files1 -J -R -B -Wthick -Cso.cpt.tmp -P -O -K >> $outps
-gmt psxy $tile_files2 -J -R -B -Wthick -Cso.cpt.tmp -P -O -K >> $outps
+#-#gunzip -c ${tile_files1}.gz > $tile_files1
+#-#gunzip -c ${tile_files2}.gz > $tile_files2
+#-#gmt psxy $tile_files1 -J -R -B -Wthick -Cso.cpt.tmp -P -O -K >> $outps
+#-#gmt psxy $tile_files2 -J -R -B -Wthick -Cso.cpt.tmp -P -O -K >> $outps
 
 # Plot predictions at WHONDRS sites
 input_file=/work/WHONDRS_S19S-SSS-log10-r02_predictions.csv
-sed 's/,/ /g' ${input_file} | awk 'NR > 1 {print $2,$3,log(-1.0*$4)/log(10.0)}' > tmp.xyr
-gmt psxy -J -R -B tmp.xyr -Gblack -Wblack -P -O -K -Sp0.2 >> $outps
-gmt psxy -J -R -B tmp.xyr -Crr.cpt.tmp -P -O -K -Sp0.15 >> $outps
+#-#sed 's/,/ /g' ${input_file} | awk 'NR > 1 {print $2,$3,log(-1.0*$4)/log(10.0)}' > tmp.xyr
+#-#gmt psxy -J -R -B tmp.xyr -Gblack -Wblack -P -O -K -Sp0.2 >> $outps
+#-#gmt psxy -J -R -B tmp.xyr -Crr.cpt.tmp -P -O -K -Sp0.15 >> $outps
 
 # Plot predictions at GLORICH sites
 # This includes predictions made at sites without correcting for elevation
@@ -69,12 +75,12 @@ gmt psxy -J -R -B tmp.xyr -Crr.cpt.tmp -P -O -K -Sp0.15 >> $outps
 #input_file=/work/RiverAtlas_GLORICH_S19S-SSS-log10-extrap-r01_predictions.csv
 # This file accounts for elevation when filling missing oxygen values
 input_file=/work/RiverAtlas_GLORICH_S19S-SSS-log10-extrap-r01_ele_corrected_O2_predictions.csv
-sed 's/,/ /g' ${input_file} | awk 'NR > 1 {print $3,$4,log(-1.0*$15)/log(10.0)}' > tmp.xyr
-gmt psxy -J -R -B tmp.xyr -Gblack -Wblack -P -O -K -Sp0.2 >> $outps
-gmt psxy -J -R -B tmp.xyr -Crr.cpt.tmp -P -O -K -Sp0.15 >> $outps
+#-#sed 's/,/ /g' ${input_file} | awk 'NR > 1 {print $3,$4,log(-1.0*$15)/log(10.0)}' > tmp.xyr
+#-#gmt psxy -J -R -B tmp.xyr -Gblack -Wblack -P -O -K -Sp0.2 >> $outps
+#-#gmt psxy -J -R -B tmp.xyr -Crr.cpt.tmp -P -O -K -Sp0.15 >> $outps
 
 # Colorbar
-gmt psscale -Dx0i/-0.75i+w5i/0.25i+e+h -Crr.cpt.tmp -Ba5g1 -B+l"Log10\ of\ Predicted\ respiration\ rate,\ mg\ O2\/L\/h" -P -O -K >> $outps
+gmt psscale -Dx0i/-0.75i+w5i/0.25i+e+h -Crr.cpt.tmp -Ba1g1 -B+l"Log\ of\ Predicted\ respiration\ rate,\ \(log10\(mg\ DO\/L\/h\)\)" -P -O -K >> $outps
 
 #===============================================
 echo Plot predictions on whole river network...
@@ -82,11 +88,11 @@ echo Plot predictions on whole river network...
 #===============================================
 
 # Background
-gmt psbasemap -JM5i -R${ropt} -Ba2/a2Wesn -P -O -K -Y4i >> $outps
-gmt grdimage /work/ETOPO1_Ice_g_gmt4.grd -J -R -B -P -O -K -Ctopo.cpt.tmp >> $outps
+#-#gmt psbasemap -JM5i -R${ropt} -Ba2/a2Wesn -P -O -K -Y4i >> $outps
+#-#gmt grdimage /work/ETOPO1_Ice_g_gmt4.grd -J -R -B -P -O -K -Ctopo.cpt.tmp >> $outps
 
 # River network plotted by predicted value
-input_file=/work/grdb_S19S-SSS-log10-extrap-r02_predictions.csv
+input_file=/work/grdb_S19S-SSS-log10-extrap-r03_predictions.csv
 gunzip -c $input_file.gz > $input_file
 awk -F, 'NR > 1 {print ">",$2,"-Z"(log(-1.0*$17)/log(10)),"\n"$3,$4,"\n"$5,$6}' $input_file | gmt psxy -J -R -B -Wthick -Crr.cpt.tmp -P -O -K >> $outps
 
